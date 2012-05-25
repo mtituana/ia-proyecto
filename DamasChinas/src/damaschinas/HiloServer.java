25ca809cb1c0bba7ffc99e6de5bb670de0a8ca2a
joseluisdmgua <joseluisdmgua@3b9cf459-b437-3664-ce53-46a3032a4064>
2012-05-25 19:58:27 +0000

    La conexión debe ser hecha sin importar el tipo de juego. Dependiendo del tipo de juego y de qué jugador es la instancia ejecutandose (jugador 1 o jugador 2) se permite hacer clic solo en las fichas propias. Ya se puede jugar human vs human.

--- DamasChinas/src/damaschinas/HiloServer.java
+++ DamasChinas/src/damaschinas/HiloServer.java
@@ -51,7 +51,7 @@ public class HiloServer extends Thread{
         while(true && Encendido){
             try{            
                 Socket cliente = socketServidor.accept();
-                System.out.println("Aceptado cliente");
+                System.out.println("Conexión entrante");
                 cliente.setSoLinger(true, 10);            
                 ObjectInputStream ois = new ObjectInputStream(cliente
                         .getInputStream());
@@ -64,34 +64,94 @@ public class HiloServer extends Thread{
                         if(m[0].compareTo("0")==0){//Se recibe la IP del cliente
                             Padre.IpCompañero=m[1];
                             System.out.println("Conexión con cliente IP = "+m[1]);
-                            Padre.setTitle("");                            
-                            Padre.MandarObjetoSocket(Padre.IpCompañero, new Object[]{1,Padre.max_piezas},Interfaz.PUERTO_CLIENTE);                            
+                            Padre.setTitle("");  
+                            //Se envian datos de la partida
+                            Padre.MandarObjetoSocket(Padre.IpCompañero, new Object[]{0,Padre.max_piezas,Padre.TipoPartida},Interfaz.PUERTO_CLIENTE);                            
                             Padre.crearTablero();
                             Padre.calcularContiguos();
                             Padre.jTextField1.setEnabled(true);
                             Padre.NumeroJugador=1;
+                            Padre.jButton1.setEnabled(true);
+                            
+                            
+                            switch(Padre.TipoPartida){
+                                case 1:{
+                                    Padre.P1_Enabled=true;
+                                    Padre.P2_Enabled=false;
+                                    break;
+                                }
+                                case 2:{
+                                    Padre.P1_Enabled=true;
+                                    Padre.P2_Enabled=false;
+                                    break;
+                                }
+                                case 3:{
+                                    Padre.P1_Enabled=false;
+                                    Padre.P2_Enabled=false;
+                                    break;
+                                }
+                            }                                                   
                         }                                                
                     }                    
                 }
                 if(mensaje instanceof Object[]){
-                    Object[] m=(Object[])mensaje;
-                    if(m.length==2){
-                        if((Integer)m[0]==1){//Se obtiene la respeusta del servidor      
-                            System.out.println("Se obtuvo respuesta de servidor = "+Padre.IpCompañero);                        
-                            Padre.setTitle("");  
-                            Padre.max_piezas=(Integer)m[1];
-                            Padre.TipoPartida=1;
-                            Padre.crearTablero();
-                            Padre.calcularContiguos();
-                            Padre.NumeroJugador=2;
-                            Padre.jButton2.setEnabled(false);
-                            Padre.jButton9.setEnabled(false);
-                            Padre.jTextField1.setEnabled(true);
+                    Object[] m=(Object[])mensaje;                    
+                    if(m.length==3){
+                        switch((Integer)m[0]){
+                            case 0:{//Se obtiene respuesta del servidor
+                                System.out.println("Se obtuvo respuesta de servidor = "+Padre.IpCompañero);                        
+                                Padre.setTitle("");  
+                                Padre.max_piezas=(Integer)m[1];
+                                Padre.TipoPartida=(Integer)m[2];
+                                Padre.crearTablero();
+                                Padre.calcularContiguos();
+                                Padre.NumeroJugador=2;
+                                Padre.jButton1.setEnabled(false);
+                                Padre.jButton2.setEnabled(false);
+                                Padre.jButton9.setEnabled(false);
+                                Padre.jTextField1.setEnabled(true);
+
+                                switch(Padre.TipoPartida){
+                                    case 1:{
+                                        Padre.P1_Enabled=false;
+                                        Padre.P2_Enabled=true;
+                                        break;
+                                    }
+                                    case 2:{
+                                        Padre.P1_Enabled=false;
+                                        Padre.P2_Enabled=false;
+                                        break;
+                                    }
+                                    case 3:{
+                                        Padre.P1_Enabled=false;
+                                        Padre.P2_Enabled=false;
+                                        break;
+                                    }
+                                }
+                                break;
+                            }
+                        }
+                    }
+                    if(m.length==5){
+                        switch((Integer)m[0]){                            
+                            case 0:{//Se obtiene movimiento de oponente                                                                
+                                System.out.println("Se obtiene movimiento oponente.");
+                                Padre.Mover(Padre.encontrarCasilla((Integer)m[1], (Integer)m[2]), 
+                                        Padre.encontrarCasilla((Integer)m[3], (Integer)m[4]),false);
+                                
+                            }
                         }
                     }
                 }
                 if(mensaje instanceof String){//Se obtiene un mensaje por chat
                     Padre.list1.add("Oponente: "+mensaje.toString());
+                }
+                if(mensaje instanceof Integer){//Se obtiene un mensaje por chat
+                    int m=(Integer)mensaje;
+                    if(m==0){
+                        System.out.println("El oponente ha finalizado su turno.");
+                        Padre.TerminarTurno(false);
+                    }
                 }
 
                 // Cierre de sockets
